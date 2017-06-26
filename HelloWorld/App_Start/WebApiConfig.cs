31d86987f491b4ad339de922b1735837e44cfcb7
Bobby Yasumura <byasumura@bdo.ca>
2017-06-26 17:23:20 -0400

    Added restier poc

---
+++ HelloWorld/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using HelloWorld.Models;
+using Microsoft.Restier.Providers.EntityFramework;
+using Microsoft.Restier.Publishers.OData;
+using Microsoft.Restier.Publishers.OData.Batch;
+using System.Web.Http;
+
+namespace HelloWorld
+{
+    public static class WebApiConfig
+    {
+        public async static void Register(HttpConfiguration config)
+        {
+            await config.MapRestierRoute<EntityFrameworkApi<AdventureWorksLT>>(
+                "AdventureWorksLT",
+                "api/AdventureWorksLT",
+                new RestierBatchHandler(GlobalConfiguration.DefaultServer));
+        }
+    }
+}
